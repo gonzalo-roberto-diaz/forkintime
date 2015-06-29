509143628ec04beef89c483bc819e3193714755f
gonzalo-roberto-diaz <gonzalo_diaz@yahoo.com>
2015-06-28 23:14:07 -0600

    issue #2 and issue#3 - remove unnecessary controllers that did partial loads - add controllers to the routes, when possible, and remove them from the div containers - improving style of home page - change "services" translations

--- javascript/routes.js
+++ javascript/routes.js
@@ -1,8 +1,11 @@
 angular.module('ForkInTime')
 .config(function($routeProvider){
-  $routeProvider.when('/service', {templateUrl: '/templates/pages/service/index.html'})
-  .when('/ourfleet', {templateUrl: '/templates/pages/ourfleet/index.html'})
-  .when('/whyus', {templateUrl: '/templates/pages/whyus/index.html'})
+  $routeProvider.when('/service',
+    {templateUrl: '/templates/pages/service/index.html'})
+  .when('/ourfleet',
+    {templateUrl: '/templates/pages/ourfleet/index.html', controller: 'ForkliftsIndexCtrl', controllerAs: 'fleetCtrl'})
+  .when('/whyus',
+    {templateUrl: '/templates/pages/whyus/index.html', controller: 'ReviewsIndexCtrl', controllerAs: 'reviewsIndexCtrl'})
   .when('/contact', {templateUrl: '/templates/pages/contact/index.html'})
   .otherwise({redirectTo: '/service'});
 });
