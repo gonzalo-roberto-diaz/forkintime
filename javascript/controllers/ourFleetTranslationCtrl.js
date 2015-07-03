eca1ef60a4ff795d2b31ed178cf0a65aae7c25c8
gonzalo-roberto-diaz <gonzalo_diaz@yahoo.com>
2015-07-03 10:18:24 -0500

    issue #2 improve style of "our services" tab issue #3 add controllers to the route directly issue #4 replace translate filters for directives issue #5 remove partial async translation table loaders

--- javascript/controllers/ourFleetTranslationCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('ForkInTime')
-.controller('OurFleetTranslationCtrl', function ($scope, $translatePartialLoader, $translate) {
-  $translatePartialLoader.addPart('fleet');
-  $translate.refresh();
-});
--- javascript/controllers/ourServicesTranslationCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('ForkInTime')
-.controller('OurServicesTranslationCtrl', function ($scope, $translatePartialLoader, $translate) {
-  $translatePartialLoader.addPart('ourServices');
-  $translate.refresh();
-});
