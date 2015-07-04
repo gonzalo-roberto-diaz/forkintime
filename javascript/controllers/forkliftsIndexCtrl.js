6f019eccb36a5e5a5f9b03d4427fed1f0cf1f72e
gdiazpromeba <dongonzalito@gmail.com>
2015-07-04 15:23:42 -0500

    #7 correct more urls

--- javascript/controllers/forkliftsIndexCtrl.js
+++ javascript/controllers/forkliftsIndexCtrl.js
@@ -1,7 +1,7 @@
 angular.module('ForkInTime')
 .controller('ForkliftsIndexCtrl', ['$http', '$scope',   function($http,  $scope){
 
-    $http({url: "/php/fleet.php" , method: 'GET'}).success(function(data){
+    $http({url: "php/fleet.php" , method: 'GET'}).success(function(data){
       $scope.forklifts = data;
     });
 
--- javascript/controllers/reviewsIndexCtrl.js
+++ javascript/controllers/reviewsIndexCtrl.js
@@ -1,5 +1,5 @@
 angular.module('ForkInTime').controller('ReviewsIndexCtrl', function($http, $scope){
-  $http.get("/php/reviews.php").success(function(data){
+  $http.get("php/reviews.php").success(function(data){
     $scope.reviews=data;
   })
 });
